ac2a70749924cb392a88037dfa79130298915a73
Anna Ianikova <yatwins90@gmail.com>
2022-04-08 21:19:26 -0500

    added search function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Open-source code, by Anna Ianikova</small>
+        </footer>
+      </div>
     </div>
   );
 }
