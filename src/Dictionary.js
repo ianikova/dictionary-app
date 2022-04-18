dccb0c2374ce88e5b5811d1ceadfb5f1b3d80cf9
Anna Ianikova <yatwins90@gmail.com>
2022-04-17 20:35:12 -0500

    added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Title from "./Title";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.default);
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   // eslint-disable-next-line
   useEffect(search, []);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event?.preventDefault();
 
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001760b581d60d041d2b4a7e771d0d3e1a1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=10`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -46,7 +58,15 @@ export default function Dictionary(props) {
         <div className="hint">i.e. sunrise, wine, book, horse </div>
       </section>
       <section>
-        <Results results={results} />
+        {results ? <Title results={results} /> : null}
+        <div class="row">
+          <div class="col-md">
+            <Results results={results} />
+          </div>
+          <div class="col-md">
+            <Photos photos={photos} />
+          </div>
+        </div>
       </section>
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Title from "./Title";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <Title results={props.results} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
